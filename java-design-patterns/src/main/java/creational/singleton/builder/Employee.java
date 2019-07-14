6a0d3e13706e795dd543516f4ef3404c677945b8
Vinoth <vinothm.manoharan@gmail.com>
2019-07-14 14:53:37 +0530

    Builder Design Pattern

---
+++ java-design-patterns/src/main/java/creational/singleton/builder/Employee.java
@@ -0,0 +1,46 @@
+package creational.singleton.builder;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private int age;
+
+	public static class Builder {
+		private int id;
+		private String name;
+		private int age;
+
+		public Builder id(int id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder name(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public Builder age(int age) {
+			this.age = age;
+			return this;
+		}
+
+		public Employee build() {
+			return new Employee(this);
+		}
+
+	}
+
+	private Employee(Builder builder) {
+		this.id = builder.id;
+		this.name = builder.name;
+		this.age = builder.age;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+
+}
---
+++ java-design-patterns/src/main/java/creational/singleton/builder/EmployeeDemo.java
@@ -0,0 +1,13 @@
+package creational.singleton.builder;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		Employee employee = new Employee.Builder().id(1).name("Java").age(24).build();
+		System.out.println(employee);
+		
+		Employee employee1 = new Employee.Builder().id(1).age(21).build();
+		System.out.println(employee1);
+	}
+
+}
